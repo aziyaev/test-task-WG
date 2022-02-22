13b8ce235c7a22490b9bd0c3ef3316294975f000
Azizbek <aziyaev99@gmail.com>
2022-02-22 16:10:17 +0300

    Refactor

--- task1.py
+++ task1.py
@@ -7,15 +7,16 @@ def isEvenBite(value):
     return value & 1
 
 def main():
+    times = 1000000
     # Первый алгоритм
     start_time = datetime.now()
-    for num in range(1000000):
+    for num in range(times):
         isEvenSimple(num)
     print(f"Division method  {datetime.now() - start_time}")
 
     # Алгоритм с битовой операцией И
     start_time = datetime.now()
-    for num in range(1000000):
+    for num in range(times):
         isEvenBite(num)
     print(f"Bit operation method {datetime.now() - start_time}")
 
